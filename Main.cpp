aa59e76e82625f4580f0cdd53b33030bf54ae780
ValentinaYun <50779948+ValentinaYun@users.noreply.github.com>
2019-05-20 20:17:19 +0300

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -8,27 +8,27 @@
 #include "Matrix.h"
 #include <ctime>
 
+
 using namespace std;
+int val;
 
 const double a = 1;
-const double c1 = 1.5;
-const double c2 = -3;
+const double c1 = 0.3;
+const double c2 = 1;
 
 const long double tMin = 0.0;
 const long double tMax = 1.0;
 const long double xMin = 0.0;
 const long double xMax = 1.0;
 
-const int xSteps = 5;
-const int tSteps = 500;
-const long double xStep = 1.0 / xSteps - 1;
-const long double tStep = 1.0 / tSteps -1 ;
-
-unsigned int exact_time;
-unsigned int approx_time;
+const int xSteps = 8;
+const int tSteps = 1000
+;
+const long double xStep = 1.0 / (xSteps -1) ;
+const long double tStep = 1.0 / (tSteps - 1);
 
 double function_w(double previousW, double currentW, double nextW, double q, double h) {
-	return currentW + q*(pow(currentW, -1.0)*((nextW - 2 * currentW + previousW) / pow(h, 2.0)) - pow(currentW, -2.0)*pow((nextW - previousW) / 2 * h, 2.0));
+	return currentW + a*q*(((1/currentW)*((nextW - 2 * currentW + previousW) / pow(h, 2.0))) - (pow(currentW, -2.0)*pow((nextW - previousW) / 2 * h, 2.0)));
 }
 
 double exactFunction(double x, double t) {
@@ -36,7 +36,7 @@ double exactFunction(double x, double t) {
 }
 
 void assessErrors(int xSteps, int tSteps, Matrix exact, Matrix approx);
-void saveToFiles(string methodName, Matrix exact, Matrix approx, int xSteps, int tSteps);
+void saveToFiles(Matrix exact, Matrix approx, int xSteps, int tSteps);
 void solve();
 
 void solve() {
@@ -48,18 +48,34 @@ void solve() {
 	long double t;
 	long double x;
 
-	unsigned int exact_start_time = clock();
-	for (i = 0, t = tMin, x = xMin; i <= tSteps; ++i, t += tStep) {
-		for (j = 0; j <= xSteps; ++j, x += xStep) {
-			exact(i, j) = exactFunction(x, t);
+	cout << "Виконати послiдовно(0) або паралельно(1)? ";
+	cin >> val;
+	unsigned int start_time_exact = clock();
+    #pragma omp parallel if (val)
+	if (omp_in_parallel()) {
+		for (i = 0, t = tMin, x = xMin; i <= tSteps; ++i, t += tStep) {
+			#pragma omp parallel for
+			for (j = 0; j <= xSteps; ++j, x += xStep) {
+				exact(i, j) = exactFunction(x, t);
+			}
+			x = xMin;
 		}
-		x = xMin;
+		cout << "Час виконання точного розв'язку паралельно: " << clock() - start_time_exact << endl;
 	}
-	unsigned int exact_end_time = clock();
-	unsigned int exact_time = exact_end_time - exact_start_time;
+	else {
+		for (i = 0, t = tMin, x = xMin; i <= tSteps; ++i, t += tStep) {
+			for (j = 0; j <= xSteps; ++j, x += xStep) {
+				exact(i, j) = exactFunction(x, t);
+			}
+			x = xMin;
+		}
+		cout << "Час виконання точного розв'язку послiдовно: " << clock() - start_time_exact << endl;
+	}
+	
+	
 
 
-	unsigned int approx_start_time = clock();
+	unsigned int start_time_approx = clock();
 	
 	for (i = 0, t = tMin; i <= tSteps; ++i, t += tStep) {
 		approx(i, 0) = exactFunction(0, t);
@@ -69,18 +85,29 @@ void solve() {
 	for (j = 0, x = xMin; j <= xSteps; ++j, x += xStep) {
 		approx(0, j) = exactFunction(x, t);
 	}
-	
-	for (i = 1; i <= tSteps; ++i) {
-#pragma omp parallel for
-		for (j = 1; j < xSteps; ++j) {
-			approx(i, j) = function_w(approx(i - 1, j - 1), approx(i - 1, j), approx(i - 1, j + 1), tStep, xStep);
+
+#pragma omp parallel if (val)
+	if (omp_in_parallel()) {
+		for (i = 1; i <= tSteps; ++i) {
+		#pragma omp parallel for
+			for (j = 1; j < xSteps; ++j) {
+				approx(i, j) = function_w(approx(i - 1, j - 1), approx(i - 1, j), approx(i - 1, j + 1), tStep, xStep);
+			}
 		}
+		cout << "Час виконання наближеного розв'язку паралельно: " << clock() - start_time_approx << endl;
 	}
-	unsigned int approx_end_time = clock();
-	unsigned int approx_time = approx_end_time - approx_start_time;
+	else {
+		for (i = 1; i <= tSteps; ++i) {
+			for (j = 1; j < xSteps; ++j) {
+				approx(i, j) = function_w(approx(i - 1, j - 1), approx(i - 1, j), approx(i - 1, j + 1), tStep, xStep);
+			}
+		}
+		cout << "Час виконання наближеного розв'язку послiдовно: " << clock() - start_time_approx << endl;
+	}
+	
 
 	assessErrors(xSteps, tSteps, exact, approx);
-	saveToFiles("Matrix", exact, approx, xSteps, tSteps);
+	saveToFiles(exact, approx, xSteps, tSteps);
 	return ;
 }
 
@@ -92,7 +119,7 @@ void assessErrors(int xSteps, int tSteps, Matrix exact, Matrix approx) {
 	for (int i = 1; i <= tSteps; ++i) {
 		for (int j = 1; j < xSteps; ++j) {
 			long double absoluteError = abs(approx(i, j) - exact(i, j));
-			long double relativeError = absoluteError / exact(i, j) * 100.0;
+			long double relativeError = abs(absoluteError / exact(i, j) * 100.0);
 			totalAbsoluteError += absoluteError;
 			totalRelativeError += relativeError;
 			maxAbsoluteError = max(maxAbsoluteError, absoluteError);
@@ -103,23 +130,21 @@ void assessErrors(int xSteps, int tSteps, Matrix exact, Matrix approx) {
 	long double averageRelativeError = totalRelativeError / (tSteps * xSteps);
 
 
-	cout << "РЎРµСЂРµРґРЅСЏ Р°Р±СЃРѕР»СЋС‚РЅР° РїРѕС…РёР±РєР° : " << averageAbsoluteError << "\n"
-	     << "РЎРµСЂРµРґРЅСЏ РІiРґРЅРѕСЃРЅР° РїРѕС…РёР±РєР° : " << averageRelativeError << "\n"
-             << "MР°РєСЃРёРјР°Р»СЊРЅР° Р°Р±СЃРѕР»СЋС‚РЅР° РїРѕС…РёР±РєР° : " << maxAbsoluteError << "\n"
-	     << "РњР°РєСЃРёРјР°Р»СЊРЅР° РІiРґРЅРѕСЃРЅР° РїРѕС…РёР±РєР° : " << maxRelativeError << "\n\n"
-	     << "Р§Р°СЃ РІРёРєРѕРЅР°РЅРЅСЏ С‚РѕС‡РЅРѕРіРѕ СЂРѕР·РІ'СЏР·РєСѓ : " << exact_time << "\n"
-	     << "Р§Р°СЃ РІРёРєРѕРЅР°РЅРЅСЏ Р·РЅР°Р№РґРµРЅРѕРіРѕ СЂРѕР·РІ'СЏР·РєСѓ : " << approx_time << endl;
+	cout << "Середня абсолютна похибка : " << fixed << setprecision(5) << averageAbsoluteError << endl;
+	cout << "Середня вiдносна похибка : " << fixed << setprecision(5) << averageRelativeError << "%" << endl;
+	cout << "Mаксимальна абсолютна похибка : " << fixed << setprecision(5) << maxAbsoluteError << endl;
+	cout << "Максимальна вiдносна похибка : " << fixed << setprecision(5) << maxRelativeError << "%\n" << endl;
 
 }
 
-void saveToFiles(string methodName, Matrix exact, Matrix approx, int xSteps, int tSteps) {
-	ofstream fout1;
-	ofstream fout2("approx.txt", ios_base::out);
-	fout1.open("exact.txt", ios_base::out);
+void saveToFiles(Matrix exact, Matrix approx, int xSteps, int tSteps) {
+	ofstream fout1("exact.txt");
+	ofstream fout2("approx.txt");
+	
 	for (int i = 0; i <= tSteps; ++i) {
 		for (int j = 0; j <= xSteps; ++j) {
-			fout1 << fixed << setprecision(5) << exact(i, j) << ",";
-			fout2 << fixed << setprecision(5) << approx(i, j) << ",";
+			fout1 << fixed << setprecision(5) << exact(i, j) << "\t";
+			fout2 << fixed << setprecision(5) << approx(i, j) << "\t";
 		}
 		fout1 << endl;
 		fout2 << endl;
@@ -130,7 +155,6 @@ void saveToFiles(string methodName, Matrix exact, Matrix approx, int xSteps, int
 
 int main(void) {
 	setlocale(LC_ALL, "Russian");
-	omp_set_num_threads(4);
 	solve();
 	system("pause");
 	return 0;
